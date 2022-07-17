b4c3ba90d21f1824d508ad97dbcf1b23a779c9de
Deyler Alvarez Fernández <70861729+DeylerAF@users.noreply.github.com>
2022-07-16 20:30:35 -0600

    Update app.js

--- app.js
+++ app.js
@@ -18,6 +18,6 @@ function divideBy()
 { 
         num1 = (document.getElementById("firstNumber").value);
         num2 = (document.getElementById("secondNumber").value);
-        document.getElementById("result").innerHTML = (num1 * num2);
+        document.getElementById("result").innerHTML = (num1 / num2);
         console.log(num1 * num2);
 }
