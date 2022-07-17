7b8424ae98c2b322e011dd4b9a57ae9d32497d82
DeylerAF <deylerafernandez@gmail.com>
2022-07-16 20:17:50 -0600

    Tipo de Cambio

---
+++ app.js
@@ -0,0 +1,23 @@
+/* 
+  INFO
+  https://www.w3resource.com/javascript-exercises/javascript-basic-exercise-10.php
+*/
+
+//Convert USD to EUR
+function multiplyBy()
+{
+        num1 = (document.getElementById("firstNumber").value);
+        num2 = (document.getElementById("secondNumber").value);
+        document.getElementById("result").innerHTML = (num1 * num2);
+
+        console.log(num1 * num2);
+
+}
+
+function divideBy()
+{ 
+        num1 = (document.getElementById("firstNumber").value);
+        num2 = (document.getElementById("secondNumber").value);
+        document.getElementById("result").innerHTML = (num1 * num2);
+        console.log(num1 * num2);
+}
